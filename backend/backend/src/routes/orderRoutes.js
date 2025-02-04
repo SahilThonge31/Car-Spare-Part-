26b9b445b281318c0d2e7fee384a674753b5c849
Sahil Thonge <sahilthonge1272@gmail.com>
2025-02-05 01:48:40 +0530

    Rotefilefixed

--- backend/backend/src/routes/orderRoutes.js
+++ backend/backend/src/routes/orderRoutes.js
@@ -1,9 +1,8 @@
 import express from "express";
-import { cancelOrder, createOrder, createSingleOrder, getUserOrders } from "../controllers/orderController.js";
+import { cancelOrder, createOrder, getUserOrders } from "../controllers/orderController.js";
 
 const router = express.Router();
 
-router.post("/singleorder", createSingleOrder);
 
 
 router.post("/createorder", createOrder);
