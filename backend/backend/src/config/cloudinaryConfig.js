cbc279ca01df13b3f7b49129f821361f731e86e1
Sahil Thonge <sahilthonge1272@gmail.com>
2025-02-05 15:47:16 +0530

    NEW UPDATE

--- backend/backend/src/config/cloudinaryConfig.js
+++ backend/backend/src/config/cloudinaryConfig.js
@@ -9,5 +9,8 @@
             api_key: process.env.API_KEY, 
             api_secret: process.env.API_SECRET
         });
+        console.log("HELLOW WORLD");
+        
+        
 
     export default cloudinary;
